3eef49f6d9825f277d39954a9bb728e0f880cd20
Michel Thomas Brodatzki <michel@brodatzki.com>
2026-03-14 23:08:48 +0100

    Finish basic secret store deployment for KA1

--- aws/services/secret_store/terraform.tf
+++ aws/services/secret_store/terraform.tf
@@ -8,12 +8,12 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
     }
   }
-}
-
-provider "aws" {
-  region = "eu-central-1"
 }
