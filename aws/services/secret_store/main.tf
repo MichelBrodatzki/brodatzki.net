17186ca7a2f93c2bf1eaabf2f1d083ce4b550154
Michel Thomas Brodatzki <michel@brodatzki.com>
2026-06-01 23:04:40 +0200

    [AWS] Add ka1 wealthfolio sa to allowed secrets readers

--- aws/services/secret_store/main.tf
+++ aws/services/secret_store/main.tf
@@ -27,7 +27,8 @@ data "aws_iam_policy_document" "ka1_oidc_workload_assume_role" {
         "system:serviceaccount:cert-manager:cert-manager-secrets-sa",
         "system:serviceaccount:n8n:n8n-secrets-sa",
         "system:serviceaccount:paperless:paperless-secrets-sa",
-        "system:serviceaccount:plex:plex-secrets-sa"
+        "system:serviceaccount:plex:plex-secrets-sa",
+        "system:serviceaccount:wealthfolio:wealthfolio-secrets-sa"
       ]
     }
   }
