f2eff998acccc50fd1a2a2ef0876cc6647413fe7
Michel Thomas Brodatzki <michel@brodatzki.com>
2026-07-04 03:47:15 +0200

    [KA1] Fix create backend ca script for kanidm

--- ka1/kube1/services/kanidm/create-backend-ca.sh
+++ ka1/kube1/services/kanidm/create-backend-ca.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 curl -s https://letsencrypt.org/certs/isrgrootx1.pem https://letsencrypt.org/certs/isrg-root-x2.pem > ca.crt
-kubectl create configmap kanidm-backend-ca -n kanidm --from-file=ca.crt --dry-run=client -o yaml
+kubectl create configmap kanidm-backend-ca -n kanidm --from-file=ca.crt --dry-run=client -o yaml | kubectl apply -f -
 rm ca.crt
