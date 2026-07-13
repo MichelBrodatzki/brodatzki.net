a9039d61b9d5d1a9899e3f86245103ea24b68b96
Michel Thomas Brodatzki <michel@brodatzki.com>
2026-07-13 18:34:16 +0200

    [AWS] Add mealie sa to secret store auth

--- aws/services/secret_store/main.tf
+++ aws/services/secret_store/main.tf
@@ -22,11 +22,11 @@ data "aws_iam_policy_document" "ka1_oidc_workload_assume_role" {
         "system:serviceaccount:mosquitto:mosquitto-secrets-sa",
         "system:serviceaccount:cert-manager:cert-manager-secrets-sa",
         "system:serviceaccount:n8n:n8n-secrets-sa",
-        "system:serviceaccount:paperless:paperless-secrets-sa",
         "system:serviceaccount:plex:plex-secrets-sa",
         "system:serviceaccount:wealthfolio:wealthfolio-secrets-sa",
         "system:serviceaccount:actualbudget:actualbudget-secrets-sa",
-        "system:serviceaccount:openwebui:openwebui-secrets-sa"
+        "system:serviceaccount:openwebui:openwebui-secrets-sa",
+        "system:serviceaccount:mealie:mealie-secrets-sa"
       ]
     }
   }
