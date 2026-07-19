92aab9ec6e1dd4a699ffcf198ab170c80a98b55c
Michel Thomas Brodatzki <michel@brodatzki.com>
2026-07-19 16:32:40 +0200

    [KA1] Add barman cloud plugin to cnpg

--- ka1/kube1/greenfield/07_cloudnativepg/01_install_cnpg.sh
+++ ka1/kube1/greenfield/07_cloudnativepg/01_install_cnpg.sh
@@ -1,2 +1,6 @@
 kubectl apply --server-side -f \
   https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.30/releases/cnpg-1.30.0.yaml
+
+kubectl apply -f \
+        https://github.com/cloudnative-pg/plugin-barman-cloud/releases/download/v0.13.0/manifest.yaml
+
